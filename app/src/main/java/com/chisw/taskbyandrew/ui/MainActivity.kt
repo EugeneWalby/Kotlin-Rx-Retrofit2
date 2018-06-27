fbef0da8c48f8844410d025cf07c7a8896ec28a3
Eugene Volboyenko <eugene.volboyenko@chisw.com>
2018-06-27 17:37:50 +0300

    Added moving to the first task activity

--- app/src/main/java/com/chisw/taskbyandrew/ui/MainActivity.kt
+++ app/src/main/java/com/chisw/taskbyandrew/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.chisw.taskbyandrew.ui
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,27 +9,48 @@ import android.widget.ArrayAdapter
 import com.chisw.taskbyandrew.R
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+    private var selectedSpinnerItem: Int? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         createListOfTasks()
+        createOnBtnOKClickListener()
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        selectedSpinnerItem = position
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        selectedSpinnerItem = null
     }
 
     private fun createListOfTasks() {
-        spTasks?.onItemSelectedListener = OnSpinnerItemSelectedListener()
-        val adapter: ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(this, R.array.titles_of_tasks, android.R.layout.simple_spinner_item)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spTasks?.onItemSelectedListener = this
+        val adapter: ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(
+                this,
+                R.array.titles_of_tasks,
+                android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spTasks?.adapter = adapter
     }
 
-    private class OnSpinnerItemSelectedListener: AdapterView.OnItemSelectedListener {
-        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+    private fun createOnBtnOKClickListener() {
+        btnOK.setOnClickListener {
+            when (selectedSpinnerItem) {
+                0 -> {
+                    val intent = Intent(this, TitlesOfStoriesActivity::class.java)
+                    startActivity(intent)
+                }
+                1 -> {
 
-        }
+                }
+                else -> {
 
-        override fun onNothingSelected(parent: AdapterView<*>?) {
-
+                }
+            }
         }
     }
 }
