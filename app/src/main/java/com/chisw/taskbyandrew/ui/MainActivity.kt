1d945e89d58b1b34f33a6d8d47c9c3ae2ad47564
Eugene Volboyenko <eugene.volboyenko@chisw.com>
2018-06-27 16:58:58 +0300

    Added new class for first task

--- app/src/main/java/com/chisw/taskbyandrew/ui/MainActivity.kt
+++ app/src/main/java/com/chisw/taskbyandrew/ui/MainActivity.kt
@@ -2,67 +2,11 @@ package com.chisw.taskbyandrew.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
-import android.view.View
-import android.widget.Toast
 import com.chisw.taskbyandrew.R
-import com.chisw.taskbyandrew.network.AlgoliaApiService
-import com.chisw.taskbyandrew.network.model.Model
-import com.chisw.taskbyandrew.ui.adapter.StoriesAdapter
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
-import io.reactivex.schedulers.Schedulers
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    companion object {
-        const val TAGS = "story"
-        const val PAGE = 1
-    }
-
-    private val algoliaApiService by lazy {
-        AlgoliaApiService.create()
-    }
-    private val storiesList: ArrayList<String> = ArrayList()
-    private var disposable: Disposable? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        loadHistory(PAGE)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        disposable?.dispose()
-    }
-
-    private fun loadHistory(page: Int) {
-        pbProcessing.visibility = View.VISIBLE
-        disposable = algoliaApiService.getStoryTitles(page, TAGS)
-                .concatWith(algoliaApiService.getStoryTitles(page + 1, TAGS))
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({ result ->
-                    fillStoriesList(result)
-                    fillRecyclerByStories()
-                    pbProcessing.visibility = View.GONE
-                },
-                        {
-                            Toast.makeText(applicationContext, getString(R.string.error_loading_stories), Toast.LENGTH_SHORT).show()
-                            pbProcessing.visibility = View.GONE
-                        }
-                )
-    }
-
-    private fun fillStoriesList(result: Model.HitsResponse) {
-        for (i in 0 until result.hits.size) {
-            storiesList.add(result.hits[i].title)
-        }
-    }
-
-    private fun fillRecyclerByStories() {
-        rvStoriesList.layoutManager = LinearLayoutManager(this)
-        rvStoriesList.adapter = StoriesAdapter(storiesList, this)
     }
 }
