a13e536832218914f9a2f3e5d8787eed33fce6f7
Eugene Volboyenko <eugene.volboyenko@chisw.com>
2018-07-01 15:47:22 +0300

    Added solution for subject task

--- app/src/main/java/com/chisw/taskbyandrew/ui/activity/task10/SubjectActivity.kt
+++ app/src/main/java/com/chisw/taskbyandrew/ui/activity/task10/SubjectActivity.kt
@@ -1,10 +1,53 @@
 package com.chisw.taskbyandrew.ui.activity.task10
 
+import android.os.Bundle
+import android.util.Log
 import com.chisw.taskbyandrew.R
 import com.chisw.taskbyandrew.ui.activity.base.BaseActivity
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.BehaviorSubject
 
+// done
 class SubjectActivity : BaseActivity() {
     override fun provideLayout(): Int {
         return R.layout.activity_subject
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        createSubject()
+    }
+
+    private fun createSubject() {
+        val list = mutableListOf<Int>()
+        val behaviorSubject: BehaviorSubject<Int> = BehaviorSubject.create()
+        behaviorSubject.onNext(1)
+        behaviorSubject.onNext(2)
+        behaviorSubject.onNext(3)
+
+        val disposable1 = behaviorSubject
+                .observeOn(Schedulers.io())
+                .subscribe(
+                        {
+                            Log.d("---", it.toString())
+                            list.add(it)
+                        }
+                )
+        addDisposable(disposable1)
+
+        behaviorSubject.onNext(4)
+        behaviorSubject.onNext(5)
+        behaviorSubject.onNext(6)
+
+        val disposable2 = behaviorSubject.observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        {
+                            for (items in list) {
+                                showToast(items.toString())
+                            }
+                        }
+                )
+        addDisposable(disposable2)
     }
 }
