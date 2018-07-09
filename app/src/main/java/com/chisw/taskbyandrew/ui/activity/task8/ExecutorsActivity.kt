4a1b8aeec170ebbaf5a43793d923f1826db5dd09
Eugene Volboyenko <eugene.volboyenko@chisw.com>
2018-07-09 12:12:11 +0300

    Changed exetutors task implementation with zipWith method

--- app/src/main/java/com/chisw/taskbyandrew/ui/activity/task8/ExecutorsActivity.kt
+++ app/src/main/java/com/chisw/taskbyandrew/ui/activity/task8/ExecutorsActivity.kt
@@ -7,6 +7,7 @@ import com.chisw.taskbyandrew.network.AlgoliaApiService
 import com.chisw.taskbyandrew.network.model.Model
 import com.chisw.taskbyandrew.ui.activity.base.BaseActivity
 import io.reactivex.Single
+import io.reactivex.functions.BiFunction
 import io.reactivex.schedulers.Schedulers
 import java.util.concurrent.Executors
 
@@ -38,21 +39,23 @@ class ExecutorsActivity : BaseActivity() {
         val disposable = algoliaApiService.getStoriesInfo(PAGE, TAGS)
                 .logThread()
                 .subscribeOn(Schedulers.from(executor1))
-                .observeOn(Schedulers.from(executor2))
+                .zipWith(algoliaApiService.getStoriesInfo(PAGE + 1, TAGS).subscribeOn(Schedulers.from(executor2)),
+                        BiFunction<Model.HitsResponse, Model.HitsResponse, List<Model.HitsStoryResponse>> { t1, t2 -> t1.hits.plus(t2.hits) })
                 .logThread()
-                .concatWith { algoliaApiService.getStoriesInfo(PAGE + 1, TAGS) }
                 .observeOn(Schedulers.from(executor3))
                 .subscribe({ t ->
                     logTitlesOfStories(t)
                     Log.d(ExecutorsActivity.LOG_TAG, getString(R.string.log_current_thread) + Thread.currentThread().name)
 
+                }, {
+                    it.printStackTrace()
                 })
         addDisposable(disposable)
     }
 
-    private fun logTitlesOfStories(result: Model.HitsResponse) {
-        for (i in 0 until result.hits.size) {
-            Log.d(LOG_TAG, result.hits[i].title)
+    private fun logTitlesOfStories(resultList: List<Model.HitsStoryResponse>) {
+        for (story in resultList) {
+            Log.d(LOG_TAG, story.title)
         }
     }
 
